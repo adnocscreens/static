0f95fac77307e7e562724d4e19c7f97a3c7c2007
Developer <irshadmelethil+adnoc@gmail.com>
2017-09-30 21:22:21 +0530

    animate in slide

--- dist/js/pages/dashboard.js
+++ dist/js/pages/dashboard.js
@@ -123,8 +123,40 @@ $(function () {
   $('#chat-box').slimScroll({
     height: '250px'
   });
-  $('.carousel').carousel({
+  var $myCarousel = $('.carousel').carousel({
     interval: 10000,
     pause: false
   })
+    //Function to animate slider captions 
+  function doAnimations( elems ) {
+    //Cache the animationend event in a variable
+    var animEndEv = 'webkitAnimationEnd animationend';
+    
+    elems.each(function () {
+      var $this = $(this),
+        $animationType = $this.data('animation');
+      $this.addClass($animationType).one(animEndEv, function () {
+        $this.removeClass($animationType);
+      });
+    });
+  }
+  
+  //Variables on page load 
+    var $firstAnimatingElems = $myCarousel.find('.item:first').find("[data-animation ^= 'animated']");
+    
+  //Initialize carousel 
+  // $myCarousel.carousel();
+  
+  //Animate captions in first slide on page load 
+  doAnimations($firstAnimatingElems);
+  
+  // //Pause carousel  
+  // $myCarousel.carousel('pause');
+  
+  
+  //Other slides to be animated on carousel slide event 
+  $myCarousel.on('slide.bs.carousel', function (e) {
+    var $animatingElems = $(e.relatedTarget).find("[data-animation ^= 'animated']");
+    doAnimations($animatingElems);
+  }); 
 });
